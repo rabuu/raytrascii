dcc3a3b757813aac912e3f2ebc41a08c5f7f2d4d
rabuu <rabuu.dev@gmail.com>
2022-03-27 21:26:20 +0200

    Implement basic orthonormal basis

--- src/lalg/mod.rs
+++ src/lalg/mod.rs
@@ -1,7 +1,9 @@
 //! Basic implementation of vectors etc
 
+mod orthnormbasis;
 mod vec;
 
+pub use orthnormbasis::OrthNormBasis3;
 pub use vec::Vec3;
 
 /// Three-dimensional point (type alias to [Vec3])
---
+++ src/lalg/orthnormbasis.rs
@@ -0,0 +1,25 @@
+use super::{Point3, Vec3};
+
+/// Three-dimensional orthonormal basis
+#[derive(Debug, Clone, PartialEq)]
+pub struct OrthNormBasis3 {
+    pub u: Vec3,
+    pub v: Vec3,
+    pub w: Vec3,
+}
+
+impl OrthNormBasis3 {
+    /// Default shorthand constructor
+    pub fn new(u: Vec3, v: Vec3, w: Vec3) -> Self {
+        OrthNormBasis3 { u, v, w }
+    }
+
+    /// Constructs an orientation orthonormal basis from two points and a "view up" vector
+    pub fn orientation(from: Point3, to: Point3, vup: Vec3) -> Self {
+        let w = (from - to).unit_vec();
+        let u = vup.cross(w).unit_vec();
+        let v = w.cross(u);
+
+        OrthNormBasis3 { u, v, w }
+    }
+}
